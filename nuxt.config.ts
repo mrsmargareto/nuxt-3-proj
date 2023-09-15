86df6e05ad14fc6c9a5b6a6cb2f66838f3d0f6cc
Margaret Ossman <mrsmargareto@gmail.com>
2023-09-15 09:47:21 -0500

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+import { createResolver } from '@nuxt/kit'
+
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  hooks: {
+    'vite:extendConfig': (config) => {
+
+      /* 
+       config.plugins.push(
+         vuetify({
+           styles: { configFile: resolve('./settings.scss') },
+         })
+       )
+       
+ */
+    }
+  },
+
+  typescript: { shim: false },
+  devtools: { enabled: true },
 })
+
+
