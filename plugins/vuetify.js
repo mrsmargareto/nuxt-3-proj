810cffe37634628e804dacc75af33cbbc3a7b614
Margaret Ossman <mrsmargareto@gmail.com>
2023-09-15 09:47:53 -0500

    Add files via upload

---
+++ plugins/vuetify.js
@@ -0,0 +1,16 @@
+// plugins/vuetify.js
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import colors from 'vuetify/lib/util/colors'
+
+export default defineNuxtPlugin(nuxtApp => {
+    const vuetify = createVuetify({
+        ssr: true,
+        components,
+        directives,
+
+    })
+
+    nuxtApp.vueApp.use(vuetify)
+})
