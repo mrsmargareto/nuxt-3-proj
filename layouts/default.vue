f1543945f2c354c2154a3ac20304b0cf55668daf
Margaret Ossman <mrsmargareto@gmail.com>
2023-10-03 09:40:26 -0500

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,34 @@
 <template>
-    <v-app>
-
-        <v-navigation-drawer v-model="drawer" class="bg-deep-purple" theme="dark" expand-on-hover rail>
-            <v-list>
-                <v-list-item prepend-icon="mdi-account" title="User Name" subtitle="user@asurion.com"></v-list-item>
-            </v-list>
-
-            <v-divider></v-divider>
-
+    <v-app id="inspire">
+        <v-navigation-drawer theme="dark" v-model="drawer">
+            <!--  -->
             <v-list density="compact" nav>
                 <!-- Vuetify supports Material Design Icons, Material Icons, Font Awesome 4 and Font Awesome 5 -->
+                <v-list-subheader>REPORTS</v-list-subheader>
+                <v-list-item v-for="(item, i) in items" :key="i" :value="item" color="primary" rounded="shaped"
+                    :to="item.route" link nuxt>
 
-                <v-list-item to="/" prepend-icon="mdi-table-large" title="Dashboard" link nuxt></v-list-item>
-                <v-list-item to="/demos" prepend-icon="mdi-table-edit" title="Edit" link nuxt></v-list-item>
-                <v-list-item to="/security" prepend-icon="mdi-security" title="Security" link></v-list-item>
-                <v-list-item to="/contact" prepend-icon="mdi-card-account-mail" title="Contact" link></v-list-item>
+                    <template v-slot:prepend>
+                        <v-icon :icon="item.icon"></v-icon>
+                    </template>
+
+                    <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item>
 
                 <!-- https://pictogrammers.com/library/mdi/ -->
             </v-list>
         </v-navigation-drawer>
 
-        <v-app-bar density="compact" color="grey-darken-3">
-
+        <v-app-bar theme="dark">
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
             <v-app-bar-title>Application</v-app-bar-title>
-
         </v-app-bar>
 
-        <v-main>
-            <!-- output the page content -->
+
+
+        <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+            <!--  -->
             <slot />
         </v-main>
     </v-app>
@@ -38,14 +38,24 @@
 import { ref } from 'vue'
 
 const drawer = ref(null)
+const items = [
+    { text: 'Dashboard', icon: 'mdi-clock', route: '/' },
+    { text: 'Edit', icon: 'mdi-account', route: '/edit' },
+    { text: 'Security', icon: 'mdi-flag', route: '/security' },
+    { text: 'Contact', icon: 'mdi-clock', route: '/contact' },
+]
 </script>
   
 <script>
 export default {
-    data: () => ({ drawer: null }),
+    data: () => ({
+        items: [
+            { text: 'Dashboard', icon: 'mdi-clock', route: '/' },
+            { text: 'Edit', icon: 'mdi-account', route: '/edit' },
+            { text: 'Security', icon: 'mdi-flag', route: '/security' },
+            { text: 'Contact', icon: 'mdi-clock', route: '/contact' },
+        ],
+    }),
 }
-</script>
-<style lang="scss">
-@use './settings';
-</style>
 
+</script>
